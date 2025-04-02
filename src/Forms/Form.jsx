a5183817ec3f4e588486c02cf2414dd330345e54
NAJIB <najib.azmi2019@gmail.com>
2025-04-02 14:45:06 +0000

    form

--- src/Forms/Form.jsx
+++ src/Forms/Form.jsx
@@ -32,30 +32,45 @@ export default function Form() {
  
   return (
     <div className="container my-5">
-      {JSON.stringify(formValues)}
-      <form>
-        <div className="form-group">
-          <label>Name</label>
-          <input type="text" id="name" className="form-control" ref={inputNameRef} />
-        </div>
-
-        <div className="form-group">
-          <label>Age</label>
-          <input type="number" id="age" className="form-control"  ref={inputAgeRef}/>
-        </div>
-
-        <div className="form-group">
-        <input type="checkbox" id="accept" className="form-check-input" ref={inputAcceptRef}/>
+    <h2>Contact Form</h2>
+    <form>
+      <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" className="form-control" ref={inputNameRef} />
+      </div>
+  
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" className="form-control"  />
+      </div>
+  
+      <div className="form-group">
+        <label htmlFor="message">Message</label>
+        <textarea id="message" className="form-control" ></textarea>
+      </div>
+  
+      <div className="form-group">
+        <label htmlFor="country">Country</label>
+        <select id="country" className="form-control" >
+          <option value="morocco">Morocco</option>
+          <option value="algeria">Algeria</option>
+          <option value="tunisia">Tunisia</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+  
+      <div className="form-group">
+        <input type="checkbox" id="accept" className="form-check-input" ref={inputAcceptRef} />
         <label htmlFor="accept">Accept our rules</label>
-          </div>
-
-        <div className="form-check">
-          <button type="button" className="btn btn-primary" onClick={handleSubmit}>
-            Save
-          </button>
-        </div>
-      </form>
-      
-    </div>
+      </div>
+  
+      <div className="form-check">
+        <button type="button" className="btn btn-primary" onClick={handleSubmit}>
+          Save
+        </button>
+      </div>
+    </form>
+  </div>
+  
   );
 }

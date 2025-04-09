de85e72609d37b28f1ed164a92ae4b9790b61762
NAJIB <najib.azmi2019@gmail.com>
2025-04-09 09:34:41 +0100

    alert

--- src/Forms/Form.jsx
+++ src/Forms/Form.jsx
@@ -1,64 +1,98 @@
-import { useState,useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 
 export default function Form() {
-  const inputNameRef=useRef()
-  const inputEmailRef=useRef()
-  const inputMessageRef=useRef()
-  const inputAcceptRef=useRef()
-  const inputCountryRef=useRef()
-  const validateForm=()=>{
-    const NameValue=inputNameRef.current.value
-    const EmailValue=inputEmailRef.current.value
-    const MessageValue=inputMessageRef.current.value
-    const CountryValue=inputCountryRef.current.value
-    const AcceptValue=inputAcceptRef.current.checked
-    console.log("Nom :",NameValue,"Email :",EmailValue,"Message :",MessageValue,"Country :",CountryValue,"Accept Condition ?",AcceptValue)
-  }
+  const inputNameRef = useRef();
+  const inputEmailRef = useRef();
+  const inputMessageRef = useRef();
+  const inputAcceptRef = useRef();
+  const inputCountryRef = useRef();
+  const validateForm = () => {
+    const NameValue = inputNameRef.current.value;
+    const EmailValue = inputEmailRef.current.value;
+    const MessageValue = inputMessageRef.current.value;
+    const CountryValue = inputCountryRef.current.value;
+    const AcceptValue = inputAcceptRef.current.checked;
+    console.log(
+      "Nom :",
+      NameValue,
+      "Email :",
+      EmailValue,
+      "Message :",
+      MessageValue,
+      "Country :",
+      CountryValue,
+      "Accept Condition ?",
+      AcceptValue
+    );
+  };
 
-  const handleSubmit=()=>{
-          validateForm()
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    validateForm();
+  };
   return (
     <div className="container my-5">
-    <h2>Contact Form</h2>
-    <form onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" className="form-control" ref={inputNameRef} />
+      <div class="alert alert-danger" role="alert">
+        <strong>Alert Heading</strong> Some Word
       </div>
-  
-      <div className="form-group">
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" className="form-control" ref={inputEmailRef} />
-      </div>
-  
-      <div className="form-group">
-        <label htmlFor="message">Message</label>
-        <textarea id="message" className="form-control" ref={inputMessageRef}></textarea>
-      </div>
-  
-      <div className="form-group">
-        <label htmlFor="country">Country</label>
-        <select id="country" className="form-control" ref={inputCountryRef} >
-          <option value="morocco">Morocco</option>
-          <option value="algeria">Algeria</option>
-          <option value="tunisia">Tunisia</option>
-          <option value="other">Other</option>
-        </select>
-      </div>
-  
-      <div className="form-group">
-        <input type="checkbox" id="accept" className="form-check-input" ref={inputAcceptRef} />
-        <label htmlFor="accept">Accept our rules</label>
-      </div>
-  
-      <div className="form-group">
+
+      <form onSubmit={handleSubmit}>
+        <h2>Contact Form</h2>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            className="form-control"
+            ref={inputNameRef}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="form-control"
+            ref={inputEmailRef}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            className="form-control"
+            ref={inputMessageRef}
+          ></textarea>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="country">Country</label>
+          <select id="country" className="form-control" ref={inputCountryRef}>
+            <option value="morocco">Morocco</option>
+            <option value="algeria">Algeria</option>
+            <option value="tunisia">Tunisia</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <input
+            type="checkbox"
+            id="accept"
+            className="form-check-input"
+            ref={inputAcceptRef}
+          />
+          <label htmlFor="accept">Accept our rules</label>
+        </div>
+
+        <div className="form-group">
           <button type="submit" className="btn btn-primary">
             Save
           </button>
         </div>
-    </form>
-  </div>
-  
+      </form>
+    </div>
   );
 }
